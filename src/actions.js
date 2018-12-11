3a89fc78087d0ffb5b723a9325bfadb34c38c3e9
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-12-11 11:12:25 +0100

    feat(actions.js) can pull into dynamical path

--- src/actions.js
+++ src/actions.js
@@ -9,6 +9,7 @@ import {
   toFolders,
   fromFolders,
   createFile,
+  localeReplacer,
 } from './utils';
 
 /**
@@ -68,19 +69,21 @@ export function pull(rootFolder, targetPath, locale, raw=false) {
     const configPath = getConfigPath();
     const projectName = getProjectName();
     const localeappKey = JSON.parse(fs.readFileSync(configPath, 'utf8'))[projectName];
-    return localeappPull(localeappKey).then(({ response, body }) => {
-      const localesArray = ymlToJson(body);
-      console.log(`Successfully pulled locales ${Object.keys(localesArray).join(', ')} from Localeapp`);
-      Object.entries(localesArray).map((l) => {
-        const ymlLocale = jsonToYml({ [l[0]]: l[1] });
-        createFile(targetPath, l[0], ymlLocale);
-      });
-      if (raw) return {};
-      const compiledLocale = fs.readFileSync(`${targetPath}/${locale}.yml`, 'utf8');
-      const updatedFolders = toFolders(rootFolder, compiledLocale, locale);
-      console.log('Folders updated');
-      return updatedFolders;
-    }).catch((err) => console.error(err));
+    return localeappPull(localeappKey)
+      .then(({ response, body }) => {
+        const localesArray = ymlToJson(body);
+        console.log(`Successfully pulled locales ${Object.keys(localesArray).join(', ')} from Localeapp`);
+        Object.entries(localesArray).map((l) => {
+          const ymlLocale = jsonToYml({ [l[0]]: l[1] });
+          createFile(targetPath, l[0], ymlLocale);
+        });
+        if (raw) return {};
+        const compiledLocale = fs.readFileSync(`${targetPath}/${locale}.yml`, 'utf8');
+        const updatedFolders = toFolders(rootFolder, compiledLocale, locale);
+        console.log('Folders updated');
+        return updatedFolders;
+      })
+      .catch((err) => console.error(err));
   }
   catch (err) {
     console.error('No localeapp project key found! Please specify one with the init command');
